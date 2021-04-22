17d7f3cfc5b22323f30beaa8782ff9ca9bbc0285
nando <muhammad.rezki.ananda@gmail.com>
2021-04-22 11:24:43 +0700

    pertemuan satu

---
+++ Bahan Ajar/pertemuan_satu.py
@@ -0,0 +1,18 @@
+# menghitung luas persegi panjang
+
+panjang = int(input("Masukkan panjang : "))
+lebar = int(input("Masukkan lebar : "))
+
+luas = panjang * lebar
+print("Luas persegi panjang adalah", luas)
+
+
+# divison dan modulus
+a = 10
+b = 3
+
+x = a // b
+y = a % b
+
+print(x)
+print(y)
