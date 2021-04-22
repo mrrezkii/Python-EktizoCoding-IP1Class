586847aace323103ab1aa0dd24c603bb2f12d9cc
nando <muhammad.rezki.ananda@gmail.com>
2021-04-22 10:51:33 +0700

    Pengumpulan tugas 05 April 2021

---
+++ Bari - 27 Maret 2021/main.py
@@ -0,0 +1,12 @@
+Januari= 100
+print(Januari)
+Februari= 200
+print(Februari)
+Maret= 300
+print(Maret)
+April= 400
+print(April)
+Total= Januari + Februari + Maret + April
+print(Total)
+Rata_Rata= Total/4
+print("Rata-rata dari tagihan bulanan listrik adalah :" , Rata_Rata)
