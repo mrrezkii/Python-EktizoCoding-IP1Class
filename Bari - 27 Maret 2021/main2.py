0d95507a1c52ed0b2c1804c0e16fab93fdee8def
nando <muhammad.rezki.ananda@gmail.com>
2021-04-22 10:55:50 +0700

    Pengumpulan tugas 10 April 2021

---
+++ Bari - 27 Maret 2021/main2.py
@@ -0,0 +1,5 @@
+nomor= int(input("masukkan bilangan:"))
+if nomor%2==0:
+    print ("bilangan",nomor,"adalah genap")
+else:
+    print ("bilangan",nomor,"adalah ganjil")
---
+++ Tobias - 03 April 2021/main.py
@@ -0,0 +1,10 @@
+januari= int(input("masukan biaya dibulan januari"))
+februari= int(input("masukan biaya dibulan februari"))
+maret= int(input("masukan biaya dibulan maret"))
+april= int(input("masukan biaya dibulan april"))
+
+jumlah=januari+februari+maret+april
+print(jumlah)
+
+rata=jumlah/4
+print("rata rata dari tagihan bulanan listrik adalah",rata)
