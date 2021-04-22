6279a2d381aa32e553022486442b0945243fa9da
nando <muhammad.rezki.ananda@gmail.com>
2021-04-22 11:36:11 +0700

    pertemuan tiga

---
+++ Bahan Ajar/pertemuan_tiga.py
@@ -0,0 +1,30 @@
+# Case Study 1
+
+for i in range(1, 11):
+    print(i)
+
+print()
+
+for j in range(10, 26, 5):
+    print(j)
+
+print()
+
+for k in range(1, 11):
+    print(k**2)
+
+print()
+
+jumlah = 0
+for l in range(1, 6):
+    jumlah += l
+print(jumlah)
+
+print()
+
+# Case Study 2
+
+for m in range(1, 31):
+    if m == 4 or m == 25:
+        continue
+    print(m)
