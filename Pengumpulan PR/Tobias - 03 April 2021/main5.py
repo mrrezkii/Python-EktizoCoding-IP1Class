b28bf06298af0d68af85cb6e6b78d7e19190ab4c
nando <muhammad.rezki.ananda@gmail.com>
2021-05-10 05:17:52 +0700

    Realiasasi tanggal 08 Mei 2021

---
+++ Pengumpulan PR/Tobias - 03 April 2021/main5.py
@@ -0,0 +1,5 @@
+waktu=00
+for jam in range (waktu,24):
+    for menit in range (waktu,60,30):
+        print(jam,":",menit)
+        
---
+++ Pengumpulan PR/Tobias - 03 April 2021/main6.py
@@ -0,0 +1,26 @@
+solo = 10000
+couple = 15000
+group = 25000
+
+no_topping = 0
+chocolate = 3000
+almond = 5000
+jelly = 6000
+total = 0
+harga = size + toping
+size = input("masukan size yang anda inginkan:")
+topping = input("masukan topping yang anda inginkan:")
+
+while total + harga :
+  if size == solo :
+    print ("harganya adalah Rp 10.000")
+  elif size == couple :  
+    print ("harganya adalah Rp 15.000")
+  elif size == group :
+    print ("harganya adalah Rp")
+  elif topping == no_topping :
+    print ("harganya adalah Rp"no_topping)
+  elif topping == chocolate :
+    print ("harganya adalah Rp",chocolate) 
+else:
+  print ("maaf toping/rasa tidak tersedia")
